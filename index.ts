f0cdca7c5f06b444239aa4527037d6a577a5c9cb
amitzed <amitzmn@gmail.com>
2020-12-22 17:17:50 -0500

    In ts-file added an interface to define the structure of the object

--- index.ts
+++ index.ts
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url)
 .then(response => {
   const todo = response.data;
