032a49cdaa6562b547c5c0e8c7b0f0c33c8985f2
amitzed <amitzmn@gmail.com>
2020-12-22 17:13:54 -0500

    Typed completed key as a variable called done to purposefully generate a failure when running ts-node index.ts

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var axios_1 = require("axios");
+var url = 'https://jsonplaceholder.typicode.com/todos';
+axios_1["default"].get(url)
+    .then(function (response) {
+    console.log(response.data);
+});
--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
 import axios from 'axios';
 
-const url = 'https://jsonplaceholder.typicode.com/todos';
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(url)
 .then(response => {
-  console.log(response.data);
+  const todo = response.data;
+
+  const ID = todo.id;
+  const title = todo.title;
+  const done = todo.done;
+
+  console.log(`
+      The Todo with ID: ${ID}
+      Has a title of: ${title}
+      Completed? ${done}
+  `);
+  
 });
